4a7f631f8770a7dbaf1fc2c124184bfcf3736e46
bijoy <bijoycse24@gmail.com>
2025-10-16 22:57:50 +0600

    navbar done

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -3,21 +3,23 @@ import { useContext } from "react";
 import { createContext } from "react";
 import { useNavigate } from "react-router-dom";
 
-export const AppContext = createContext();
+ const AppContext = createContext();
 
-export const AppContextProvider = ({ children }) => {
+ const AppContextProvider = ({ children }) => {
     const navigate = useNavigate();
-    const [user, setUser] = useState(null);
+    const [user, setUser] = useState(true);
     const [isSeller, setIsSeller] = useState(null);
+    const [showUserLogin, setShowUserLogin] = useState(false);
 
     const value = {
-        navigate,user,setUser,isSeller,setIsSeller
+        navigate,user,setUser,isSeller,setIsSeller,showUserLogin, setShowUserLogin
     }
     
     return <AppContext.Provider value={value}>
         {children}
     </AppContext.Provider>
 }
+export default AppContextProvider;
 
 export const useAppContext = () => {
     return useContext(AppContext);
