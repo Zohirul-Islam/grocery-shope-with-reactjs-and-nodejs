3b56421d087d654ec21e5799fed7deb6cbc525a3
bijoy <bijoycse24@gmail.com>
2025-10-21 20:45:49 +0600

    fix some issue

--- frontend/src/pages/ProductCategory.jsx
+++ frontend/src/pages/ProductCategory.jsx
@@ -9,7 +9,8 @@ const ProductCategory = () => {
   const { category } = useParams();
   const searchCategory = categories.find(
     (item) => item.path.toLowerCase() === category
-  );
+    );
+    
   const filteredProducts = products.filter(
     (product) => product.category.toLowerCase() === category
   );
@@ -31,7 +32,7 @@ const ProductCategory = () => {
         </div>
       ) : (
         <div className="flex items-center justify-center h-[60vh]">
-          <p>No products found in this category</p>
+          <p className="text-2xl font-medium text-primary">No products found in this category</p>
         </div>
       )}
     </div>
