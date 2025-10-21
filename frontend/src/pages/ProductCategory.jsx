f2025e4fa0677bacc76b1422819099d687283490
bijoy <bijoycse24@gmail.com>
2025-10-21 20:27:19 +0600

    fix category

--- frontend/src/pages/ProductCategory.jsx
+++ frontend/src/pages/ProductCategory.jsx
@@ -30,7 +30,9 @@ const ProductCategory = () => {
           ))}
         </div>
       ) : (
-        <div></div>
+        <div className="flex items-center justify-center h-[60vh]">
+          <p>No products found in this category</p>
+        </div>
       )}
     </div>
   );
