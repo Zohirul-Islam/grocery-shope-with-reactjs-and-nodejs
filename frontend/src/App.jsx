a539e0251797f75cb87e886d20d0564998a37f71
bijoy <bijoycse24@gmail.com>
2025-10-22 21:31:56 +0600

    add dummy cart

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,6 +11,7 @@ import { useAppContext } from "./context/AppContext";
 import Allproducts from "./pages/Allproducts";
 import ProductCategory from "./pages/ProductCategory";
 import ProductDetails from "./pages/ProductDetails";
+import Cart from "./pages/Cart";
 
 const App = () => {
   const { showUserLogin } = useAppContext();
@@ -34,6 +35,7 @@ const App = () => {
           <Route path="/products" element={<Allproducts/>} />
           <Route path="/products/:category" element={<ProductCategory/>} />
           <Route path="/products/:category/:id" element={<ProductDetails/>} />
+          <Route path="/cart" element={<Cart/>} />
         </Routes>
       </div>
       {!isSellerPath && <Footer/>}
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -50,7 +50,7 @@ const Navbar = () => {
         </div>
 
         {!user ? (
-          <button onClick={()=>setShowUserLogin(true)} className="cursor-pointer px-8 py-2 bg-primary-dull hover:bg-indigo-600 transition text-white rounded-full">
+          <button onClick={()=>setShowUserLogin(true)} className="cursor-pointer px-8 py-2 bg-primary hover:bg-primary-dull transition text-white rounded-full">
             Login
           </button>
         ) : (
@@ -63,15 +63,27 @@ const Navbar = () => {
           </div>
         )}
       </div>
-
-      <button
+      <div className="flex items-center gap-6 sm:hidden">
+         <div onClick={()=>navigate('/cart')} className="relative cursor-pointer">
+          <img
+            src={assets.nav_cart_icon}
+            alt="cart-icon"
+            className="w-6 opacity-80"
+          />
+          <button className="absolute -top-2 -right-3 text-xs text-white bg-primary w-[18px] h-[18px] rounded-full">
+            {getCartCount()}
+          </button>
+        </div>
+        <button
         onClick={() => (open ? setOpen(false) : setOpen(true))}
         aria-label="Menu"
-        className="sm:hidden"
+        className=""
       >
         {/* Menu Icon */}
         <img src={assets.menu_icon} alt="menu-icon" />
       </button>
+      </div>
+
 
       {/* Mobile Menu */}
       {open && (
